677c91ee8696db2d37ca354be247dd76adc60e1b
cperez9525nov-netizen <cperez9525nov@gmail.com>
2025-12-01 16:42:00 +0100

    Add files via upload

---
+++ AlertSender.java
@@ -0,0 +1,20 @@
+import java.io.FileWriter;
+public class AlertSender {
+    private static final String EMERGENCY_NUMBER = "112";
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Protocolo de llamada de emergencia 112");
+
+        if (confirm.equals("S")) {
+            simulateCall(location, description);
+        } else {
+            System.out.println("Protocolo de llamada cancelado por el usuario");
+        }
+        scanner.close();
+
+        public static void simulateCall() {
+            System.out.println("Llamada al número: " + EMERGENCY_NUMBER);
+        }
+    }
+}
